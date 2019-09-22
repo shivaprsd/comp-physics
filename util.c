09f1fede2e1ba55b88211f57643c544ffb078d15
Shiva Prasad <shivaprsdv@gmail.com>
2019-09-22 09:59:52 +0530

    Add dependency lib: util

---
+++ util.c
@@ -0,0 +1,110 @@
+/* Utilities for genral purpose programming */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "util.h"
+
+/* Dynamically allocate an n element array of doubles */
+double *vector(int n)
+{
+	double *v;
+
+	v = (double *) malloc((size_t) n * sizeof(double));
+	return v;
+}
+
+/* Print the elements of an array of size n */
+void print_vec(double *v, int n)
+{
+	for (; n > 0; --n)
+		printf("%.7lf\t", *v++);
+	putchar('\n');
+}
+
+/* Copy array s to t, both having size n */
+void copy_vec(double *s, double *t, int n)
+{
+	for (; n > 0; --n)
+		*t++ = *s++;
+}
+
+/* Calculate the n dimensional distance between two vectors */
+double dist(double *v1, double *v2, int n)
+{
+	double d;
+
+	for (d = 0.0; n > 0; --n)
+		d += pow((*v1++ - *v2++), 2);
+	return sqrt(d);
+}
+
+/* Return a random real number between lb and ub (both inclusive) */
+double frand(double lb, double ub)
+{
+	int i;
+	static int p = -1;
+	static int deck[DECK_SIZE];
+
+	if (p < 0) {
+		srand(time(NULL));
+		for (i = -8; i < DECK_SIZE; ++i)
+			if (i >= 0) deck[i] = rand();
+		p = rand();
+	}
+	i = (int) (p / (RAND_MAX + 1.0) * DECK_SIZE);
+	p = deck[i];
+	deck[i] = rand();
+	return (ub - lb) * p / RAND_MAX + lb;
+}
+
+/* Compare two real numbers; tolerance can be defined as EPS */
+enum bool eq(double x, double y)
+{
+	if (fabs(x - y) < EPS)
+		return true;
+	else
+		return false;
+}
+
+/* n dimensional RK4 algorithm for calculating a single step.
+ * This implementation is loosely based on the one given in Numerical Recipes.
+ * As we cannot multiply arrays with numbers in C (without using a loop), an
+ * approach different from the traditional way (k1, k2, ...) has been followed.
+ */
+void rk4(double x, double y[], int n, deriv_func f, double h, double yout[])
+{
+	int i;
+	double hh, xh, *dy, *dys, *yt;
+
+	yt = vector(n);
+	dy = vector(n);		/* derivatives of y1, y2, ... */
+	dys = vector(n);	/* sum of derivatives */
+	hh = 0.5 * h;
+	xh = x + hh;
+
+	f(x, y, dy);				/* first step */
+	for (i = 0; i < n; ++i) {
+		yt[i] = y[i] + hh * dy[i];	/* y + k1 / 2 */
+		dys[i] = dy[i];
+	}
+	f(xh, yt, dy);				/* second step */
+	for (i = 0; i < n; ++i) {
+		yt[i] = y[i] + hh * dy[i];	/* y + k2 / 2 */
+		dys[i] += 2 * dy[i];
+	}
+	f(xh, yt, dy);				/* third step */
+	for (i = 0; i < n; ++i) {
+		yt[i] = y[i] + h * dy[i];	/* y + k3 */
+		dys[i] += 2 * dy[i];
+	}
+	f(x + h, yt, dy);			/* fourth step */
+	for (i = 0; i < n; ++i)
+		yout[i] = y[i] + h / 6.0 * (dys[i] + dy[i]);	/* final y */
+
+	free(yt);
+	free(dy);
+	free(dys);
+}
+
---
+++ util.h
@@ -0,0 +1,24 @@
+/* Utilities for general purpose programming */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+#define EPS 1e-5
+#define DECK_SIZE 32
+
+/* Function pointer to f(x, y1, y2...) */
+typedef void (*deriv_func)(double, double [], double []);
+enum bool { false, true };
+
+double *vector(int n);
+void print_vec(double *v, int n);
+void copy_vec(double *s, double *t, int n);
+
+double dist(double *v1, double *v2, int n);
+double frand(double lb, double ub);
+enum bool eq(double x, double y);
+
+void rk4(double x, double y[], int n, deriv_func f, double h, double yout[]);
+
+#endif
